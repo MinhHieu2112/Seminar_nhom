f96b24a5b0e37837cf61e6427cfce1849705fcea
MinhHieu2112 <149816357+MinhHieu2112@users.noreply.github.com>
2026-01-20 21:51:22 +0700

    setup test

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PortfolioModule } from './portfolio/portfolio.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PortfolioModule],
+  imports: [
+    PortfolioModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    PortfolioModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
